1223a2b9f31ddfbe1e25848919a5f6555fd1d426
joe-akeem <j.lengacher@gmx.net>
2020-06-09 21:04:34 +0200

    the mtb layer also contains the OSM id

--- layers/mtb/layer.sql
+++ layers/mtb/layer.sql
@@ -1,7 +1,7 @@
 
 CREATE OR REPLACE FUNCTION layer_mtb(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, class text, mtb_scale text, sac_scale text, surface text, width text, name text) AS $$
-    SELECT geometry, class, mtb_scale, sac_scale, surface, width, name
+RETURNS TABLE(osm_id bigint, geometry geometry, class text, mtb_scale text, sac_scale text, surface text, width text, name text) AS $$
+    SELECT osm_id, geometry, class, mtb_scale, sac_scale, surface, width, name
     FROM osm_mtb_linestring
     WHERE zoom_level >= 10 AND geometry && bbox;
 $$ LANGUAGE SQL IMMUTABLE;
