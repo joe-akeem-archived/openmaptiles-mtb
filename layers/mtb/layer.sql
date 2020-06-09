fabff96118ede58221faa230bc1f73b4b70ac3f9
joe-akeem <j.lengacher@gmx.net>
2020-06-09 21:23:36 +0200

    Drop function before re-creating. "Replace" doesn't work if the signature has changed.

--- layers/mtb/layer.sql
+++ layers/mtb/layer.sql
@@ -1,4 +1,4 @@
-
+DROP FUNCTION layer_mtb(geometry,integer);
 CREATE OR REPLACE FUNCTION layer_mtb(bbox geometry, zoom_level int)
 RETURNS TABLE(osm_id bigint, geometry geometry, class text, mtb_scale text, sac_scale text, surface text, width text, name text) AS $$
     SELECT osm_id, geometry, class, mtb_scale, sac_scale, surface, width, name
